6c5f0e040eb6e626446e931e284642532956693c
Maksim <plysheuski.maksim@gmail.com>
2022-04-05 14:58:41 +0400

    add grid to css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 
 
 function App() {
   return (
     <div className="App">
-        <Technologies />
-        <Header />
+        <div className="app-wrapper">
+            <header className="header">
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQWXaDd7COVSlym_qmyXEr0nLSa34w31RN0g&usqp=CAU'/>
+            </header>
+            <nav className="nav">
+                <a><div>Profile</div></a>
+                <a><div>Messages</div></a>
+                <a><div>News</div></a>
+                <a><div>Music</div></a>
+                <a><div>Settings</div></a>
+            </nav>
+            <div className="content">
+                <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlCeVhPcF0B061dWx6Y2p6ZshztnAoVQI59g&usqp=CAU"/></div>
+                <div> ava + description </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>Post1</div>
+                        <div>Post2</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
