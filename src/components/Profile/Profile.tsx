0fa44ea4c0b73d722367ae86b7ecf719b5a132af
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-04-28 15:19:02 +0400

    create Dialogs page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 function Profile() {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlCeVhPcF0B061dWx6Y2p6ZshztnAoVQI59g&usqp=CAU"/>
             </div>
