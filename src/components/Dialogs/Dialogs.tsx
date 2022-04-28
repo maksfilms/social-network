8ab9acc799e05936786d17e577ddb5cf3e7509ba
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-04-28 22:29:06 +0400

    add dialogs into messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,37 @@
 import React from "react";
-import s from './Dialogs.module.css';
+import s from "./Dialogs.module.css";
 
 export function Dialogs() {
-    return(
-        <div>
-            Dialogs
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Bob
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    John
+                </div>
+                <div className={s.dialog}>
+                    David
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>
+                    Hello
+                </div>
+                <div className={s.dialog}>
+                    How are you?
+                </div>
+                <div className={s.dialog}>
+                    Fine
+                </div>
+            </div>
         </div>
     )
 }
