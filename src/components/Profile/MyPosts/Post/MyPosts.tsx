03585cf51ab54390f052e10d23ee0110159600c8
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-05-18 16:55:23 +0300

    add data refactoring

--- src/components/Profile/MyPosts/Post/MyPosts.tsx
+++ src/components/Profile/MyPosts/Post/MyPosts.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post";
-import {MessageType} from "./Post";
 
 
 function MyPosts() {
 
-    const postData: Array<MessageType> = [
+    const postData = [
         {id: 1, message: "Hi! how are you?", likesCount: 12},
-        {id: 1, message: "My first post", likesCount: 13},
+        {id: 2, message: "My first post", likesCount: 3},
+        {id: 3, message: "My first post", likesCount: 1},
+        {id: 4, message: "My first post", likesCount: 1},
+        {id: 5, message: "My first post", likesCount: 13},
     ]
 
     return (
@@ -23,7 +25,7 @@ function MyPosts() {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postData}/>
+                <Post postData={postData[0].message} likesCount={postData[0].likesCount}/>
             </div>
         </div>
     )
